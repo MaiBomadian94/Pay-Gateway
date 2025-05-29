18002f9e29cf2cbcbc5f722a7893076a21187b96
MaiBomadian94 <Maibomadian@gmail.com>
2025-05-29 16:17:24 +0300

    add thank you view widgets

--- lib/features/payment/presentation/views/thank_you_view.dart
+++ lib/features/payment/presentation/views/thank_you_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:payment_integration/core/utlis/styles.dart';
+import 'package:payment_integration/features/payment/presentation/widgets/total_price_widget.dart';
+
+import '../widgets/custom_cart_order_info.dart';
+import '../widgets/positioned_dash_row.dart';
 
 class ThankYouView extends StatelessWidget {
   const ThankYouView({super.key});
@@ -44,19 +49,81 @@ class ThankYouViewBody extends StatelessWidget {
               backgroundColor: Colors.white,
             ),
           ),
-           Positioned(
-            top: -20,
+          Positioned(
+            top: -50,
             right: 0,
             left: 0,
             child: CircleAvatar(
-
+              radius: 50,
               backgroundColor: const Color(0xffD9D9D9),
               child: CircleAvatar(
+                radius: 40,
                 backgroundColor: const Color(0xff34A853),
-                child:SvgPicture.asset('assets/images/check.svg',fit: BoxFit.scaleDown,) ,
+                child: SvgPicture.asset(
+                  'assets/images/check.svg',
+                  fit: BoxFit.scaleDown,
+                  width: 50,
+                  height: 28,
+                ),
               ),
             ),
           ),
+          Positioned(
+            top: 66,
+            right: 0,
+            left: 0,
+            child: Column(
+              children: [
+                const Text(
+                  'Thank you!',
+                  style: Styles.textStyle25,
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  'Your transaction was successful',
+                  style: Styles.textStyle20,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 42,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 22),
+                  child: Column(
+                    children: [
+                      CustomOrderInfo(
+                        title: 'Date',
+                        value: '01/24/2023',
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        child: CustomOrderInfo(
+                          title: 'Time',
+                          value: '10:15 AM',
+                        ),
+                      ),
+                      CustomOrderInfo(
+                        title: 'To',
+                        value: 'Sam Louis',
+                      ),
+                      SizedBox(height: 30,),
+                       Divider(
+                        height: 34,
+                        thickness: 2,
+                        color: Color(0xffC7C7C7),
+                      ),
+                      SizedBox(height: 24,),
+                      TotalPrice(title: 'Total', value: r'$50.97')
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const PositionedDashRow(),
         ],
       ),
     );
